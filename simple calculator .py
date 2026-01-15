c20f864811821e4b357a9275033351cdba6db460
jeevikaravidran <jeevikaravindran05@gmail.com>
2026-01-15 19:48:39 +0530

    Add files via upload

---
+++ simple calculator .py
@@ -0,0 +1,19 @@
+x=int(input("Enter a number1:"))
+y=int(input("Enter a number2:"))
+z=input("Enter a function you want to perform-->")
+if z=="addition" :
+    print("the sum is",x+y)
+elif z=="subtraction":
+    print("the difference is",x-y)
+elif z=="multiplication":
+    print("the prroduct is",x*y)
+elif z=="division" : 
+    print("the division ",x/y)
+else :
+    print("the num is not devisible ")
+
+
+    
+
+
+
